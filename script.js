a82e63e31d844cfa86bfd784c0040222de7d36b7
unknown <zenfonezd551@gmail.com>
2021-10-27 20:23:55 +0300

    add scropt.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    }
+  });
